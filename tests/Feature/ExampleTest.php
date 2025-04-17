a2a5fc411e66882a8fc4c1569aa37f1efbbb22f8
Ibtissam Noukta <ibtissam.noukta@gmail.com>
2025-04-17 23:40:34 +0100

    #6 ddddd

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -8,7 +8,7 @@ use Tests\TestCase;
 class ExampleTest extends TestCase
 {
     /*
-     * A basic test example. hhh
+     * A basic test example.
      */
     public function test_the_application_returns_a_successful_response(): void
     {
